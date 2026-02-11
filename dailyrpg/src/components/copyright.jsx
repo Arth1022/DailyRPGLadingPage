20e1ed433cbfa381a5279e50316a3afb067e0f1e
Arthur Alexandre <arthurteixeira.1337@gmail.com>
2026-02-11 00:26:48 -0300

    Update copyright.jsx

--- dailyrpg/src/components/copyright.jsx
+++ dailyrpg/src/components/copyright.jsx
@@ -3,9 +3,13 @@ function Copyright() {
     <div className="flex flex-col items-center justify-center  ">
       <p className="text-white text-sm text-center">
         © 2025 DailyRPG. <br />
-        Este é um projeto pessoal sem fins lucrativos e não está associado a nenhuma entidade oficial. <br />
-        as imagens utilizadas são de propriedade de seus respectivos donos. <br />
-        
+        Este é um projeto pessoal sem fins lucrativos e não está associado a
+        nenhuma entidade oficial. <br />
+        as imagens utilizadas são de propriedade de seus respectivos donos.{" "}
+        <br />
+        NÃO HÁ VERSÃO FINAL DISPONÍVEL PARA DOWNLOAD. <br />O JOGO FOI
+        DESENVOLVIDO MAS NÃO POSSUI SERVIDORES NO MOMENTO E TODAS AS IMAGENS SÃO
+        APENAS PARA FINS DE PREVIEW.
       </p>
     </div>
   );
